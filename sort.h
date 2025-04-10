5dcfa004706077c4407eb521258fd7c08206b57f
Alejandro <10653@holbertonstudents.com>
2025-04-10 18:44:47 +0000

    saved arch

--- sort.h
+++ sort.h
@@ -16,6 +16,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
