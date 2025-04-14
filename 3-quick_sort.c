009cb6d6b97545f5f460592797b6d31a4aad4f9d
Nahuel Bica <nahuelbica5912@gmail.com>
2025-04-14 00:19:54 +0000

    task 3, quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,2 +1,83 @@
 #include "sort.h"
+/**
+ * swap - Swap 2 elements of array
+ * @in1: element 1
+ * @in2: element 2
+ *
+ * Description: This function swaps two numbers of array
+ *
+ * Return: Nothing.
+ */
+void swap(int *in1, int *in2, int arr[], size_t size)
+{
+	if (*in1 != *in2)
+	{
+		int temp = *in1;
+		*in1 = *in2;
+		*in2 = temp;
+		print_array(arr, size);
+	}
+}
+/**
+ * partition - order a partition of array
+ * @arr: array of disordered numbers
+ * @low: min index of partition
+ * @high: max index of partition
+ * @size: size of array
+ *
+ * Description: order a partition and return index of pivot
+ *
+ * Return: index of pivot.
+ */
+int partition(int arr[], int low, int high, size_t size)
+{
+	int pivot = arr[high];
+	int contEx = (low - 1), contIn;
+
+	for (contIn = low; contIn < high; contIn++)
+	{
+		if (arr[contIn] <= pivot)
+		{
+			contEx++;
+			swap(&arr[contEx], &arr[contIn], arr, size);
+
+		}
+	}
+	swap(&arr[contEx + 1], &arr[high], arr, size);
+	return (contEx + 1);
+}
+/**
+ * qSort_recursive - Sort an array using quick algorithm
+ * @arr: array of disordered numbers
+ * @low: min index of array
+ * @high: max index of array
+ * @size: size of array
+ *
+ * Description: This function calls partition and calls themselve to subarrays
+ *
+ * Return: Nothing.
+ */
+void qSort_recursive(int arr[], int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high, size);
+
+		qSort_recursive(arr, low, pi - 1, size);
+		qSort_recursive(arr, pi + 1, high, size);
+	}
+}
+/**
+ * quick_sort - Sort an array using quick algorithm
+ * @array: array of disordered numbers
+ * @size: size of array
+ *
+ * Description: This function is used to call a recursive func
+ *
+ * Return: Nothing.
+ */
+void quick_sort(int *array, size_t size)
+{
+	qSort_recursive(array, 0, (int)size - 1, size);
+}
 
